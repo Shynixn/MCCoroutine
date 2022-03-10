2259a6f058f4ac2cc8e734ec86ad2c00d791118c
shynixn <shynixn@email.com>
2022-03-10 20:20:43 +0100

    #50 Implemented sample plugin.

---
+++ mccoroutine-bungeecord-sample/src/main/java/com/github/shynixn/mccoroutine/sample/impl/FakeDatabase.kt
@@ -0,0 +1,24 @@
+package com.github.shynixn.mccoroutine.sample.impl
+
+import com.github.shynixn.mccoroutine.sample.entity.UserData
+import net.md_5.bungee.api.connection.ProxiedPlayer
+
+class FakeDatabase {
+    /**
+     *  Simulates a getUserData call to a real database by delaying the result.
+     */
+    fun getUserDataFromPlayer(player: ProxiedPlayer): UserData {
+        Thread.sleep(5000)
+        val userData = UserData()
+        userData.amountOfEntityKills = 20
+        userData.amountOfPlayerKills = 30
+        return userData
+    }
+
+    /**
+     * Simulates a save User data call.
+     */
+    fun saveUserData(userData: UserData) {
+        Thread.sleep(6000)
+    }
+}
