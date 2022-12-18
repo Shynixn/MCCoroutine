2e23dd0d2b475ec4b09b6a9848d138d70f7875aa
shynixn <shynixn@email.com>
2022-12-18 14:45:04 +0100

    #78,87 Added documentation about the shutdown strategy.

--- mccoroutine-bukkit-api/src/main/java/com/github/shynixn/mccoroutine/bukkit/ShutdownStrategy.kt
+++ mccoroutine-bukkit-api/src/main/java/com/github/shynixn/mccoroutine/bukkit/ShutdownStrategy.kt
@@ -1,5 +1,8 @@
 package com.github.shynixn.mccoroutine.bukkit
 
+/**
+ * See https://shynixn.github.io/MCCoroutine/wiki/site/plugindisable for more details.
+ */
 enum class ShutdownStrategy {
     /**
      * Default shutdown strategy. The coroutine session is
