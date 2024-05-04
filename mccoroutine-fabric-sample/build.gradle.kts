c120f64ad2efc33b1708b563b7edbc5a67923eb3
shynixn <shynixn@email.com>
2024-05-05 00:09:39 +0200

    #113 Updated to release version.

--- mccoroutine-fabric-sample/build.gradle.kts
+++ mccoroutine-fabric-sample/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
     mavenLocal()
 }
 dependencies {
-    implementation("com.github.shynixn.mccoroutine:mccoroutine-fabric-api:2.15.0")
-    implementation("com.github.shynixn.mccoroutine:mccoroutine-fabric-core:2.15.0")
+    implementation("com.github.shynixn.mccoroutine:mccoroutine-fabric-api:2.16.0")
+    implementation("com.github.shynixn.mccoroutine:mccoroutine-fabric-core:2.16.0")
 
     minecraft("com.mojang", "minecraft", project.extra["minecraft_version"] as String)
     mappings("net.fabricmc", "yarn", project.extra["yarn_mappings"] as String, null, "v2")
